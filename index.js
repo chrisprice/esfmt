7eff9eb3bb670ff1f2f2118e81ec5499ecfafb8a
Chris Price <price.c@gmail.com>
2013-12-04 21:58:14 +0000

    Add clinical mode and fix matching non-expressions

--- index.js
+++ index.js
@@ -9,16 +9,27 @@ var argv = require('optimist')
 	.usage('$0 [flags] [path ...]')
 	.options('r', 'rule')
 	.describe('r', 'Apply the rewrite rule to the source before reformatting.')
+	.options('c', 'clinical')
+	.describe('c', 'Attempt a clinical replacement of matched rules rather than reformatting the entire file')
+	.boolean('c')
 	.argv;
 
+function parseRulePart(part) {
+	var node = parse(part).body[0];
+	// allow expressions to match regardless of their location
+	if (node.type === 'ExpressionStatement') {
+		node = node.expression;
+	}
+	return node;
+}
 
 
 if (argv.r) {
 	var ruleParts = argv.r.split('->');
 	var rule = {
 		raw: argv.r,
-		pattern: parse(ruleParts[0]).body[0],
-		replacement: parse(ruleParts[1]).body[0]
+		pattern: parseRulePart(ruleParts[0]),
+		replacement: parseRulePart(ruleParts[1])
 	};
 
 	input(argv, function(error, input) {
@@ -28,12 +39,14 @@ if (argv.r) {
 		var ast = parse(input, {range: true, tokens: true, comment: true});
 		ast = escodegen.attachComments(ast, ast.comments, ast.tokens);
 
+		var substitutions = [];
 		var cloner = Cloner(), walker = Walker({ 
 			recurse: function(node, key, parent) {
 				var wildcardMatcher = WildcardMatcher();
 				if (wildcardMatcher.match(rule.pattern, node)) {
 					var replacement = cloner.clone(rule.replacement);
 					var newNode = wildcardMatcher.apply(replacement);
+					substitutions.push({ replacement: newNode, original: node });
 					parent[key] = newNode;
 				} else {
 					walker.walk(node, key, parent);
@@ -44,7 +57,18 @@ if (argv.r) {
 		// TODO won't replace root
 		walker.walk(ast);
 
-		var output = escodegen.generate(ast, {comment: true});
+		var output;
+		if (argv.c) {
+			output = substitutions.reduceRight(function(source, sub) {
+				console.log(sub.original.range);
+				var replacementSource = escodegen.generate(sub.replacement);
+				return source.substring(0, sub.original.range[0]) +
+					escodegen.generate(sub.replacement) +
+					source.substring(sub.original.range[1]);
+			}, input);
+		} else {
+			output = escodegen.generate(ast, {comment: true});
+		}
 		console.log(output);
 	});
 }
